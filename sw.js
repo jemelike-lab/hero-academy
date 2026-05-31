2dd21f6f227855519a7ce90de3eb36ce463560cc
jemelike-lab <jemelike@gmail.com>
2026-05-31 03:03:30 -0400

    fix: strip HTML tags from scaffold text before TTS + SW v16

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "hero-academy-v15";
+const CACHE_VERSION = "hero-academy-v16";
 const CORE = [
   "./", "./index.html", "./number-lab.html", "./cauldron-cafe.html",
   "./word-tower.html",
