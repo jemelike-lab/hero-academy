ae5ec653a978292e505b0f9050a9ff6fca027633
jemelike-lab <jemelike@gmail.com>
2026-06-04 19:00:57 -0400

    feat: Number Lab problem readout + Cauldron/Diner Humphrey voice (SW v74)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "hero-academy-v73";
+const CACHE_VERSION = "hero-academy-v74";
 const CORE = [
   "./", "./index.html", "./number-lab.html", "./cauldron-cafe.html",
   "./word-tower.html",
